1d9b5f7d9bf26cb5464c4d863a2dfdb26f4f1836
srinivas039 <srinivas.n0037@gmail.com>
2025-12-28 20:07:13 +0530

    commit

--- front-end/service/api.js
+++ front-end/service/api.js
@@ -2,11 +2,14 @@ import axios from "axios";
 
 const BASE_URL = "https://ai-driven-supply-chain-risk-monitor-3.onrender.com/api";
 
-export const getShipments = () =>
-  axios.get(`${BASE_URL}/shipments`);
+export const getShipments = () => {
+  return axios.get(`${BASE_URL}/shipments`);
+};
 
-export const createShipment = (data) =>
-  axios.post(`${BASE_URL}/shipments`, data);
+export const createShipment = (data) => {
+  return axios.post(`${BASE_URL}/shipments`, data);
+};
 
-export const analyzeRisk = (id, data) =>
-  axios.post(`${BASE_URL}/risk/${id}`, data);
+export const analyzeRisk = (id) => {
+  return axios.post(`${BASE_URL}/risk/${id}`);
+};
