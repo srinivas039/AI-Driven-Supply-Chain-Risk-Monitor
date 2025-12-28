b3ed976301220fd11fb0b76092415b820c4ead2e
srinivas039 <srinivas.n0037@gmail.com>
2025-12-28 13:46:40 +0530

    commit

--- front-end/src/Component/RiskMap.jsx
+++ front-end/src/Component/RiskMap.jsx
@@ -1,75 +1,35 @@
-import { MapContainer, TileLayer, CircleMarker, Popup, Polyline } from "react-leaflet";
+import { MapContainer, TileLayer } from "react-leaflet";
+import HeatmapLayer from "react-leaflet-heatmap-layer-v3";
 import "leaflet/dist/leaflet.css";
+import countryCoordinates from "../utils/countryCoordinates";
 
 function RiskMap({ shipments }) {
-  if (!shipments || shipments.length === 0) {
-    return <p>No data for map</p>;
-  }
-
-  const mapShipments = shipments.filter(
-    s => s.originCoords && s.destinationCoords
-  );
-
-  if (mapShipments.length === 0) {
-    return <p>No analyzed shipments to display on map</p>;
-  }
-
-  const getColor = (risk) => {
-    if (risk === "High") return "red";
-    if (risk === "Medium") return "orange";
-    return "green";
-  };
+  const heatmapData = shipments
+    .filter(s => s.riskScore && countryCoordinates[s.origin])
+    .map(s => {
+      const coords = countryCoordinates[s.origin];
+      return [
+        coords.lat,
+        coords.lon,
+        s.riskScore * 10
+      ];
+    });
 
   return (
     <div style={{ height: "400px" }}>
       <MapContainer center={[20, 0]} zoom={2} style={{ height: "100%" }}>
-        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
 
-        {mapShipments.map(s => {
-          const route = [
-            [s.originCoords.lat, s.originCoords.lon],
-            [s.destinationCoords.lat, s.destinationCoords.lon]
-          ];
-
-          const color = getColor(s.riskLevel);
-
-          return (
-            <div key={s._id}>
-              {/* Route Line */}
-              <Polyline
-                positions={route}
-                color={color}
-                weight={3}
-                opacity={0.8}
-              />
-
-              {/* Origin Marker */}
-              <CircleMarker
-                center={[s.originCoords.lat, s.originCoords.lon]}
-                radius={8}
-                color={color}
-              >
-                <Popup>
-                  <strong>{s.supplierName}</strong><br />
-                  Origin: {s.origin}<br />
-                  Risk: {s.riskLevel}
-                </Popup>
-              </CircleMarker>
-
-              {/* Destination Marker */}
-              <CircleMarker
-                center={[s.destinationCoords.lat, s.destinationCoords.lon]}
-                radius={8}
-                color={color}
-              >
-                <Popup>
-                  Destination: {s.destination}<br />
-                  ETA: {s.totalETA?.toFixed(2)} days
-                </Popup>
-              </CircleMarker>
-            </div>
-          );
-        })}
+        <HeatmapLayer
+          fitBoundsOnLoad
+          fitBoundsOnUpdate
+          points={heatmapData}
+          longitudeExtractor={m => m[1]}
+          latitudeExtractor={m => m[0]}
+          intensityExtractor={m => m[2]}
+        />
       </MapContainer>
     </div>
   );
